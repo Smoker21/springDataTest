ad0e3db33df607b6fc25e6e8440749de77307382
smoker21 <chen.lance@gmail.com>
2013-08-23 06:39:18 +0800

    add test sources

---
+++ jersey2/src/main/java/com/raintysoft/sample/HsqlDbContextListener.java
@@ -0,0 +1,20 @@
+package com.raintysoft.sample;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class HsqlDbContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {	
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
